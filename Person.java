ecdd2ef2e6a743e85c6c3454e69afa23337330cb
Harshal Kangralkar <kangralkarharshal@gmail.com>
2025-04-15 10:59:36 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,19 @@
+package pac1;
+
+public class Person {
+	int age;
+	String name; 
+	String qauli;
+	String address;
+	void Read()
+	{
+		System.out.println("the person reads");
+	}
+	
+	void swim()
+	{
+		System.out.println("the person swims");
+	}
+	
+
+}
